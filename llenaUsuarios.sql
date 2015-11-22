83ef4979c77b9b5d7861bd1071c9a9ad426cbdbd
KuzuriX <miguecotogarcia@Migues-MacBook-Pro.local>
2015-11-22 16:33:34 -0600

    llenar usuarios

---
+++ llenaUsuarios.sql
@@ -0,0 +1,5 @@
+INSERT ALL 
+	INTO usuarios (id, nombre, apellido, correo) VALUES (1,'Miguel','Coto','mcoto@ucenfotec.ac.cr')
+	INTO usuarios (id, nombre, apellido, correo) VALUES (2,'Byron','Serrano','bserrano@ucenfotec.ac.cr')
+	INTO usuarios (id, nombre, apellido, correo) VALUES (3,'Jose','Cerdas','jcerdas@ucenfotec.ac.cr')
+;
