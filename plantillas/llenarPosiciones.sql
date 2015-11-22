3f9d52ab80e32845007b4f892ad3709b7694c4a5
Byrons94 <bironjosue@hotmail.com>
2015-11-22 17:59:28 +0100

    reparado llena posiciones

--- plantillas/llenarPosiciones.sql
+++ plantillas/llenarPosiciones.sql
@@ -1,26 +1,59 @@
 Declare
 filaJ      number(2) := 1;
-columnaJ   number(2) := 0;
-cuadranteJ number(2);
+columnaJ   number(2) := 1;
+cuadranteJ number(2) := 1;
+contador   number(10);
+val        number(3);
+
 Begin
 
-	FOR cuadranteJ in 1..81 LOOP
-		columnaJ := columnaJ+1;
-		
-		IF columnaJ = 10 then 
-			columnaJ := 1;
-		END IF;
-		
-		INSERT INTO POSICIONES
+	FOR contador in 1..81 LOOP
+
+cuadranteJ := CASE 
+ 				WHEN columnaJ <= 3
+ 			     THEN CASE 
+ 			     			WHEN filaJ <= 3 
+ 			     				THEN 1
+ 			     			WHEN filaJ <= 6 
+ 			     				THEN 4
+ 			     			WHEN filaJ <= 9 
+ 			     				THEN 7	
+ 			     			END
+ 			    WHEN columnaJ <= 6
+ 			     THEN CASE 
+ 			     			WHEN filaJ <= 3 
+ 			     				THEN 2
+ 			     			WHEN filaJ <= 6 
+ 			     				THEN 5
+ 			     			WHEN filaJ <= 9 
+ 			     				THEN 8	
+ 			     			END
+ 			    WHEN columnaJ <= 9
+ 			      THEN CASE 
+ 			     			WHEN filaJ <= 3 
+ 			     				THEN 3
+ 			     			WHEN filaJ <= 6 
+ 			     				THEN 6
+ 			     			WHEN filaJ <= 9 
+ 			     				THEN 9	
+ 			     			END 
+ 			     END;
+
+ 	INSERT INTO POSICIONES
 			(ID, FILA, COLUMNA, CUADRANTE)
 		VALUES
-			(cuadranteJ, filaJ, columnaJ, cuadranteJ); 
+			(contador, filaJ, columnaJ, cuadranteJ);
+		      
+		IF columnaJ = 9 then 
+			columnaJ := 1;
+		ELSE 	
+			columnaJ   := columnaJ + 1;
+		END IF;
 
-		IF MOD(cuadranteJ, 9) = 0 THEN 
+		IF MOD(contador, 9) = 0 THEN 
 			filaJ := filaJ+1;
 		END IF;
-		
-		
+
 	END LOOP;
 
 End;
