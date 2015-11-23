7814e438db91586661e19155f53f5c1a3736296b
joalcego <jo_cego@hotmail.com>
2015-11-22 18:01:47 -0600

    RESUSITO MIGUEL!!!

--- calcularProbabilidades.sql
+++ calcularProbabilidades.sql
@@ -1,6 +1,41 @@
-CREATE OR REPLACE PROCEDURE CalcularProbabilidades (idPartida INTEGER) AS
-	
+CREATE OR REPLACE PROCEDURE CalcularProbabilidades (pidPartida INTEGER) AS
+	totalIncognitas INTEGER := 0;
 	BEGIN
+		
+		--por cada incognita
+		FOR incognita IN (
+			SELECT tinc.* 
+			FROM incognitas tinc 
+			WHERE tinc.idPartida = pidPartida AND tinc.valor IS NULL
+		) LOOP
+			
+			--seleccionar total incognitas x fila
+			SELECT COUNT(1)
+			FROM (
+				SELECT tin.idPosicion, tin.valor
+				FROM incognitas tin 
+				UNION
+				SELECT tpis.idPosicion, tpis.valor
+				FROM pistas tpis
+			) casillas
+			WHERE casillas.idPosicion IN
+			(
+				SELECT tpo.id 
+				FROM posiciones tpo 
+				WHERE tpo.fila = 
+				(
+					SELECT tpos.fila
+					FROM incognitas tinc 
+					INNER JOIN posiciones tpos ON tpos.id = tinc.idPosicion
+				)
+			) AND casillas.valor IS NOT NULL;
+			
+				
+			END LOOP;
+			
+			
+		END LOOP;
+		
 		
 	END;
 /
